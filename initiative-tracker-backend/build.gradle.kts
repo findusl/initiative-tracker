fba2040ab2f664e8c384570985c0df195b854a3e
Sebastian <dev@findusl.de>
2022-09-08 22:40:30 +0200

    App is deployed on google app engine.
    gradle task appengineDeploy still doesn't work correctly, but manually running the gcloud app deploy command that it wants to run, does the trick.

--- initiative-tracker-backend/build.gradle.kts
+++ initiative-tracker-backend/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.google.cloud.tools.gradle.appengine.appyaml.AppEngineAppYamlExtension
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -5,31 +7,43 @@ val logback_version: String by project
 plugins {
 	application
 	kotlin("jvm") version "1.7.10"
-	id("io.ktor.plugin") version "2.1.0"
 	id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10"
+	id("com.google.cloud.tools.appengine") version "2.4.2"
+	id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "de.lehrbaum"
-version = "0.0.1"
-application {
-	mainClass.set("de.lehrbaum.ApplicationKt")
+version = "1"
 
-	val isDevelopment: Boolean = project.ext.has("development")
-	applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+application {
+	mainClass.set("io.ktor.server.netty.EngineMain")
 }
 
 repositories {
 	mavenCentral()
 }
 
+configure<AppEngineAppYamlExtension> {
+	val projectVersion = project.version.toString()
+	val projectName = project.name
+	stage {
+		setArtifact("build/libs/$projectName-${project.version}-all.jar")
+	}
+	deploy {
+		projectId = "GCLOUD_CONFIG"
+		version = projectVersion
+		stopPreviousVersion = true
+		promote = true
+	}
+}
+
 dependencies {
-	implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-	implementation("io.ktor:ktor-server-call-logging-jvm:$ktor_version")
-	implementation("io.ktor:ktor-server-http-redirect-jvm:$ktor_version")
-	implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-	implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
-	implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+	implementation("io.ktor:ktor-server-core:$ktor_version")
+	implementation("io.ktor:ktor-server-netty:$ktor_version")
+	implementation("io.ktor:ktor-server-call-logging:$ktor_version")
+	implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+	implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
 	implementation("ch.qos.logback:logback-classic:$logback_version")
-	testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+	testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- initiative-tracker-backend/settings.gradle.kts
+++ initiative-tracker-backend/settings.gradle.kts
@@ -1,1 +1,16 @@
 rootProject.name = "initiative-tracker-backend"
+
+pluginManagement {
+	repositories {
+		gradlePluginPortal()
+		mavenCentral()
+		maven("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
+	}
+	resolutionStrategy {
+		eachPlugin {
+			if (requested.id.id.startsWith("com.google.cloud.tools.appengine")) {
+				useModule("com.google.cloud.tools:appengine-gradle-plugin:${requested.version}")
+			}
+		}
+	}
+}
