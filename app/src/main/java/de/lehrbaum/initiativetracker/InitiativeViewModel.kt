75d99f6f881036be8f8e7ecf0861231837f67f4c
Sebastian <findusl@users.noreply.github.com>
2022-10-09 17:28:35 +0200

    Ask user whether he wants to save pending changes when switching sele… (#30)
    Ask user whether he wants to save pending changes when switching selection.

--- app/src/main/java/de/lehrbaum/initiativetracker/InitiativeViewModel.kt
+++ app/src/main/java/de/lehrbaum/initiativetracker/InitiativeViewModel.kt
@@ -1,6 +1,9 @@
 package de.lehrbaum.initiativetracker
 
-import androidx.lifecycle.*
+import androidx.lifecycle.MutableNonNullLiveData
+import androidx.lifecycle.NonNullLiveData
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import de.lehrbaum.initiativetracker.bestiary.BestiaryNetworkClient
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.SharingStarted
@@ -11,7 +14,7 @@ import kotlinx.coroutines.plus
 
 private const val DEFAULT_COMBATANT_TITLE = "New Combatant"
 
-class InitiativeViewModel : ViewModel() {
+class InitiativeViewModel : DelegatingViewModel<InitiativeViewModel.Delegate>() {
 
 	private var nextId = 0L
 
@@ -37,28 +40,49 @@ class InitiativeViewModel : ViewModel() {
 	val combatants: NonNullLiveData<List<CombatantViewModel>>
 		get() = _combatants
 
+	/**
+	 * Set by the adapter. Should probably hide behind functions but easier for now.
+	 */
+	var currentlyEditingCombatant: CombatantViewModel? = null
+
+	private var selectedCombatantId: Long? = null
+
 	init {
 		addCombatant()
+		latestName = "New Combatant 2"
 		addCombatant()
 	}
 
-	fun selectCombatant(selectedCombatant: CombatantViewModel?) {
-		_combatants.value = _combatants.value.map {
-			val shouldBeSelected = it.id == selectedCombatant?.id
-			if (it.editMode != shouldBeSelected) it.copy(editMode = shouldBeSelected) else it
+	fun selectCombatant(combatantToSelect: CombatantViewModel?) {
+		if (combatantToSelect?.editMode == true) return // nothing to do
+		checkIfShouldSave()
+		// could block so that the user still sees his changes in the background. Use coroutines. Future improvements
+		selectedCombatantId = combatantToSelect?.id
+		_combatants.value = _combatants.value.copyWithCorrectStates()
+	}
+
+	private fun checkIfShouldSave() {
+		val currentlyEditingCombatant = currentlyEditingCombatant ?: return
+		val oldSelectedCombatantViewModel = _combatants.value.first { it.id == currentlyEditingCombatant.id }
+		val sanitizedEditingCombatant = currentlyEditingCombatant
+			.copy(active = oldSelectedCombatantViewModel.active, editMode = oldSelectedCombatantViewModel.editMode)
+		if (sanitizedEditingCombatant != oldSelectedCombatantViewModel) {
+			delegate?.showSaveChangesDialog {
+				updateCombatant(sanitizedEditingCombatant.copy(editMode = false))
+			}
 		}
 	}
 
 	fun addCombatant() {
-		val newCombatant = CombatantViewModel(nextId++, latestName ?: DEFAULT_COMBATANT_TITLE, 99)
+		val newCombatant = CombatantViewModel(nextId++, latestName ?: DEFAULT_COMBATANT_TITLE, -10)
 		_combatants.value = (_combatants.value + newCombatant)
 			.sortedDescending()
-			.copyWithCorrectActiveState()
+			.copyWithCorrectStates()
 	}
 
 	fun updateCombatant(updatedCombatant: CombatantViewModel) {
 		_combatants.value = _combatants.value.map {
-      if (it.id == updatedCombatant.id) {
+			if (it.id == updatedCombatant.id) {
 				if (it.name != updatedCombatant.name) {
 					latestName = updatedCombatant.name
 				}
@@ -68,28 +92,32 @@ class InitiativeViewModel : ViewModel() {
 			}
 		}
 			.sortedDescending()
-			.copyWithCorrectActiveState()
+			.copyWithCorrectStates()
 	}
 
 	fun nextTurn() {
 		activeCombatantIndex = (activeCombatantIndex + 1) % combatants.value.size
-		_combatants.value = _combatants.value.copyWithCorrectActiveState()
+		_combatants.value = _combatants.value.copyWithCorrectStates()
 	}
 
 	fun prevTurn() {
 		activeCombatantIndex--
 		if (activeCombatantIndex < 0) activeCombatantIndex = combatants.value.size - 1
-		_combatants.value = _combatants.value.copyWithCorrectActiveState()
+		_combatants.value = _combatants.value.copyWithCorrectStates()
 	}
 
-	private fun List<CombatantViewModel>.copyWithCorrectActiveState(): List<CombatantViewModel> {
+	private fun List<CombatantViewModel>.copyWithCorrectStates(): List<CombatantViewModel> {
 		return this.mapIndexed { index, combatantViewModel ->
 			val shouldBeActive = activeCombatantIndex == index
-			if (shouldBeActive != combatantViewModel.active)
-				combatantViewModel.copy(active = shouldBeActive)
+			val shouldBeSelected = combatantViewModel.id == selectedCombatantId
+			if (shouldBeActive != combatantViewModel.active || shouldBeSelected != combatantViewModel.editMode)
+				combatantViewModel.copy(active = shouldBeActive, editMode = shouldBeSelected)
 			else
 				combatantViewModel
 		}
 	}
-}
 
+	interface Delegate {
+		fun showSaveChangesDialog(onOkListener: () -> Unit)
+	}
+}
