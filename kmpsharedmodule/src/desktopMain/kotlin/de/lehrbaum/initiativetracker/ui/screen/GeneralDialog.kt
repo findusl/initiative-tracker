ee80b41634d3c0dd1a6bb6e992ae504d4b5a1ec2
Sebastian Lehrbaum <sebastian@lehrbaum.de>
2023-05-01 23:54:49 +0200

    Add Menu items

--- kmpsharedmodule/src/desktopMain/kotlin/de/lehrbaum/initiativetracker/ui/screen/GeneralDialog.kt
+++ kmpsharedmodule/src/desktopMain/kotlin/de/lehrbaum/initiativetracker/ui/screen/GeneralDialog.kt
@@ -1,9 +1,15 @@
 package de.lehrbaum.initiativetracker.ui.screen
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.Dialog
 
 @Composable
 actual fun GeneralDialog(onDismissRequest: () -> Unit, content: @Composable () -> Unit) {
     Dialog(onCloseRequest = onDismissRequest) { content() }
+}
+
+@Composable
+actual fun FullscreenDialog(onDismissRequest: () -> Unit, content: @Composable () -> Unit) {
+    Text("Fullscreen dialog does not work on desktop")
 }
