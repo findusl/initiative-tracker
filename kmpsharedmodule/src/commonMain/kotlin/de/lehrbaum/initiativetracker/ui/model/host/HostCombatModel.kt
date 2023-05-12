e4831882f1c17b6407ca1b9219513032536bf0c7
Sebastian Lehrbaum <sebastian@lehrbaum.de>
2023-05-12 23:23:20 +0200

    Implement Hosting remote combat

--- kmpsharedmodule/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/model/host/HostCombatModel.kt
+++ kmpsharedmodule/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/model/host/HostCombatModel.kt
@@ -3,22 +3,23 @@ package de.lehrbaum.initiativetracker.ui.model.host
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.State
+import de.lehrbaum.initiativetracker.bl.HostConnectionState
 import de.lehrbaum.initiativetracker.ui.model.CombatantViewModel
 import de.lehrbaum.initiativetracker.ui.model.SnackbarState
 import de.lehrbaum.initiativetracker.ui.model.SwipeResponse
 import de.lehrbaum.initiativetracker.ui.model.edit.EditCombatantModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.StateFlow
-import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.flow.Flow
 
 @Stable
-interface HostCombatModel: CoroutineScope {
-    val editCombatantModel: State<EditCombatantModel?>
-    val combatants: StateFlow<List<CombatantViewModel>>
+interface HostCombatModel {
+	val hostConnectionState: Flow<HostConnectionState>
+    val combatants: Flow<List<CombatantViewModel>>
+	val editCombatantModel: State<EditCombatantModel?>
     val assignDamageCombatant: MutableState<CombatantViewModel?>
     val snackbarState: MutableState<SnackbarState?>
     val combatStarted: Boolean
     val isSharing: Boolean
+	val sessionId: Int
 
     fun onCombatantPressed(combatantViewModel: CombatantViewModel)
     fun onCombatantLongPressed(combatant: CombatantViewModel)
@@ -27,9 +28,10 @@ interface HostCombatModel: CoroutineScope {
     fun onDamageDialogSubmit(damage: Int)
     fun onAddNewPressed()
     fun nextCombatant()
+    fun previousCombatant()
     fun undoDelete()
     fun startCombat()
     fun onShareClicked()
-    fun onStopShareClicked()
-    fun showSessionId()
+	fun closeSession()
+	fun showSessionId()
 }
---
+++ kmpsharedmodule/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/model/host/HostLocalCombatModelImpl.kt
@@ -0,0 +1,22 @@
+package de.lehrbaum.initiativetracker.ui.model.host
+
+import de.lehrbaum.initiativetracker.bl.HostConnectionState
+import kotlinx.coroutines.flow.flowOf
+
+class HostLocalCombatModelImpl: HostCombatModelBase() {
+	override val hostConnectionState = flowOf(HostConnectionState.Connected)
+	override val isSharing = false
+	override val sessionId = -1
+
+	override fun onShareClicked() {
+		TODO("Not yet implemented")
+	}
+
+	override fun closeSession() {
+		throw IllegalStateException("It should not be possible")
+	}
+
+	override fun showSessionId() {
+		throw IllegalStateException("It should not be possible")
+	}
+}
