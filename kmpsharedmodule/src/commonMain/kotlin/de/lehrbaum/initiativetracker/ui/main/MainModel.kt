9a03d8424f56631888791183dc3f4c6a72e7dbc9
Sebastian <dev@findusl.de>
2023-05-21 22:46:01 +0200

    Error still persists after removing these screens

--- kmpsharedmodule/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/main/MainModel.kt
+++ kmpsharedmodule/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/main/MainModel.kt
@@ -1,8 +1,6 @@
 package de.lehrbaum.initiativetracker.ui.main
 
 import androidx.compose.runtime.Stable
-import de.lehrbaum.initiativetracker.ui.character.CharacterListModel
-import de.lehrbaum.initiativetracker.ui.client.ClientCombatModel
 import de.lehrbaum.initiativetracker.ui.host.HostCombatModel
 import kotlinx.coroutines.flow.Flow
 
@@ -12,22 +10,16 @@ interface MainModel {
 	val drawerItems: Flow<List<DrawerItem>>
 	val content: ContentState
 
-	fun onDrawerItemSelected(item: DrawerItem) { TODO() }
+	fun onDrawerItemSelected(item: DrawerItem)
 }
 
 sealed interface DrawerItem {
 	val name: String
-	object JoinCombat: DrawerItem {
-		override val name = "Join Combat"
+	object OtherModel: DrawerItem {
+		override val name = "Other Model"
 	}
 	object HostCombat: DrawerItem {
 		override val name = "Host Combat"
-	}
-	object HostExistingCombat: DrawerItem {
-		override val name = "Host existing Combat"
-	}
-	object Characters: DrawerItem {
-		override val name = "Characters"
 	}
 	data class RememberedCombat(
 		val id: Int,
@@ -39,7 +31,4 @@ sealed interface DrawerItem {
 sealed interface ContentState {
 	object Empty: ContentState
 	data class HostCombat(val hostCombatModel: HostCombatModel): ContentState
-	data class JoinCombat(val onJoin: (Int) -> Unit, val onCancel: () -> Unit, val asHost: Boolean): ContentState
-	data class ClientCombat(val clientCombatModel: ClientCombatModel): ContentState
-	data class CharacterScreen(val characterListModel: CharacterListModel) : ContentState
 }
--- kmpsharedmodule/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/main/MainModelImpl.kt
+++ kmpsharedmodule/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/main/MainModelImpl.kt
@@ -5,8 +5,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import de.lehrbaum.initiativetracker.bl.data.CombatLink
 import de.lehrbaum.initiativetracker.bl.data.CombatLinkRepository
-import de.lehrbaum.initiativetracker.ui.character.CharacterListModelImpl
-import de.lehrbaum.initiativetracker.ui.client.ClientCombatModelImpl
 import de.lehrbaum.initiativetracker.ui.host.HostLocalCombatModelImpl
 import de.lehrbaum.initiativetracker.ui.host.HostSharedCombatModelImpl
 import kotlinx.coroutines.flow.map
@@ -15,10 +13,8 @@ class MainModelImpl: MainModel {
 	override var activeDrawerItem by mutableStateOf<DrawerItem>(DrawerItem.HostCombat)
 
 	private val defaultDrawerItems = listOf(
-		DrawerItem.JoinCombat,
+		DrawerItem.OtherModel,
 		DrawerItem.HostCombat,
-		DrawerItem.HostExistingCombat,
-		DrawerItem.Characters
 	)
 
 	override val drawerItems = CombatLinkRepository.combatLinks.map { combatLinks ->
@@ -33,14 +29,12 @@ class MainModelImpl: MainModel {
     override fun onDrawerItemSelected(item: DrawerItem) {
 		if (item == activeDrawerItem) return // avoid double click race conditions
         val newContent: ContentState = when (item) {
-            is DrawerItem.Characters -> ContentState.CharacterScreen(CharacterListModelImpl())
             is DrawerItem.HostCombat -> hostCombatState
-            is DrawerItem.HostExistingCombat -> joinCombat(asHost = true)
-            is DrawerItem.JoinCombat -> joinCombat(asHost = false)
             is DrawerItem.RememberedCombat -> {
-				if(item.isHost) hostCombat(item.id) else clientCombat(item.id)
+				hostCombat(item.id)
 			}
-        }
+			DrawerItem.OtherModel -> ContentState.Empty
+		}
 		activeDrawerItem = item
 		content = newContent
 	}
@@ -52,26 +46,11 @@ class MainModelImpl: MainModel {
         return ContentState.HostCombat(hostCombatModel)
     }
 
-	private fun clientCombat(sessionId: Int): ContentState.ClientCombat {
-		val model = ClientCombatModelImpl(sessionId) {
-			onDrawerItemSelected(DrawerItem.HostCombat)
-		}
-		return ContentState.ClientCombat(model)
-	}
-
 	private fun hostCombat(sessionId: Int): ContentState.HostCombat {
 		val hostCombatModel = HostSharedCombatModelImpl(sessionId){
 			onDrawerItemSelected(DrawerItem.HostCombat)
 		}
 		return ContentState.HostCombat(hostCombatModel)
-	}
-
-	private fun joinCombat(asHost: Boolean): ContentState.JoinCombat {
-		return ContentState.JoinCombat(
-			onJoin = { switchToCombat(it, asHost) },
-			onCancel = { onDrawerItemSelected(DrawerItem.HostCombat) },
-			asHost
-		)
 	}
 
 	private fun switchToCombat(sessionId: Int, asHost: Boolean) {
