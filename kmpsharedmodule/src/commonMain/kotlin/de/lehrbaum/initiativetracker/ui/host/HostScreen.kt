d6c6f128888d548eefbb474b8f458abf2173ba75
Sebastian Lehrbaum <sebastian@lehrbaum.de>
2023-05-21 23:00:48 +0200

    Error still persists after removing this code

--- kmpsharedmodule/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/host/HostScreen.kt
+++ kmpsharedmodule/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/host/HostScreen.kt
@@ -9,9 +9,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import de.lehrbaum.initiativetracker.bl.HostConnectionState
-import de.lehrbaum.initiativetracker.ui.composables.*
+import de.lehrbaum.initiativetracker.ui.composables.BurgerMenuButtonForDrawer
 import de.lehrbaum.initiativetracker.ui.edit.EditCombatantDialog
-import de.lehrbaum.initiativetracker.ui.icons.FastForward
 import kotlinx.coroutines.launch
 
 @Composable
@@ -19,56 +18,18 @@ import kotlinx.coroutines.launch
 @ExperimentalFoundationApi
 fun HostScreen(drawerState: DrawerState, hostCombatModel: HostCombatModel) {
 	val connectionStateState = hostCombatModel.hostConnectionState.collectAsState(HostConnectionState.Connecting)
-	val scaffoldState = rememberScaffoldState()
-	scaffoldState.snackbarHostState.bindSnackbarState(hostCombatModel.snackbarState)
-	// For some reason the lambda would not be changed even though a parameter of it changed. This seems to have fixed it.
-	val topBarLambda = rememberComposableLambda(hostCombatModel) { TopBar(drawerState, hostCombatModel) }
 
 	Scaffold(
-		scaffoldState = scaffoldState,
-		topBar = topBarLambda,
-		floatingActionButton = rememberComposableLambda(hostCombatModel) {
-				NextCombatantButton(
-					hostCombatModel.combatStarted,
-					hostCombatModel::nextCombatant
-				)
-			}
+		topBar = { TopBar(drawerState, hostCombatModel) },
 	) {
 		val connectionState = connectionStateState.value
-		when (connectionState) {
-			HostConnectionState.Connected -> {
-				CombatantList(
-					hostCombatModel.combatants.collectAsState(emptyList()).value,
-					hostCombatModel::onCombatantClicked,
-					hostCombatModel::onCombatantLongClicked,
-					hostCombatModel::onAddNewPressed,
-					dismissToStartAction = swipeToDeleteAction(hostCombatModel::deleteCombatant)
-				)
-			}
-
-			HostConnectionState.Connecting -> Text("Connecting")
-			is HostConnectionState.Disconnected -> Text("Disconnected! Reason: ${connectionState.reason}")
-		}
+		if (connectionState != HostConnectionState.Connecting)
+			Text("Content")
 	}
 
 	if (connectionStateState.value == HostConnectionState.Connected) {
-		hostCombatModel.assignDamageCombatant.value?.let {
-			DamageCombatantDialog(hostCombatModel::onDamageDialogSubmit) {
-				hostCombatModel.assignDamageCombatant.value = null
-			}
-		}
-
 		hostCombatModel.editCombatantModel.value?.let {
 			EditCombatantDialog(it)
-		}
-	}
-}
-
-@Composable
-private fun NextCombatantButton(combatStarted: Boolean, onClicked: () -> Unit) {
-	if (combatStarted) {
-		FloatingActionButton(onClick = { onClicked() }) {
-			Icon(Icons.Default.FastForward, contentDescription = "Next Combatant")
 		}
 	}
 }
