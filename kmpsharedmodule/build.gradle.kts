296bdb105ac5f3784603f8dcf1bcba9186fc14ec
Sebastian <dev@findusl.de>
2023-05-21 23:51:28 +0200

    Error still persists after refactoring like this

--- kmpsharedmodule/build.gradle.kts
+++ kmpsharedmodule/build.gradle.kts
@@ -1,13 +1,7 @@
-import com.codingfeline.buildkonfig.compiler.FieldSpec.Type.INT
-import com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING
-import java.util.*
 
 plugins {
 	kotlin("multiplatform")
 	id("org.jetbrains.compose") version Version.JetbrainsCompose.foundation
-	id("org.jetbrains.kotlin.plugin.serialization")
-	// Used to mimic BuildConfig on Multiplatform
-	id("com.codingfeline.buildkonfig") version Version.buildKonfig
 }
 
 kotlin {
@@ -23,18 +17,7 @@ kotlin {
 				implementation(compose.runtime)
 				implementation(compose.preview)
 
-				implementation("com.russhwolf:multiplatform-settings:${Version.mppSettings}")
-				implementation("com.russhwolf:multiplatform-settings-serialization:${Version.mppSettings}")
-
-				implementation(Dependency.kotlinxSerialization)
 				implementation(Dependency.kotlinxCoroutines)
-
-				implementation("io.ktor:ktor-client-core:${Version.ktor}")
-				implementation("io.ktor:ktor-client-serialization:${Version.ktor}")
-				implementation("io.ktor:ktor-client-content-negotiation:${Version.ktor}")
-				implementation("io.ktor:ktor-serialization-kotlinx-json:${Version.ktor}")
-				implementation("io.ktor:ktor-client-websockets:${Version.ktor}")
-				implementation("io.ktor:ktor-client-logging-jvm:${Version.ktor}")
 
 				// Multiplatform Logging
 				api(Dependency.napier)
@@ -48,42 +31,7 @@ kotlin {
 		named("desktopMain") {
 			dependencies {
 				implementation(compose.desktop.common)
-				implementation("io.ktor:ktor-client-okhttp:${Version.ktor}")
 			}
 		}
-	}
-}
-
-// part of BuildKonfig plugin
-buildkonfig {
-	val localProperties = Properties()
-	val localPropertiesFile: File = project.rootProject.file("local.properties")
-	if (localPropertiesFile.exists()) {
-		localPropertiesFile.inputStream().use {
-			localProperties.load(it)
-		}
-	}
-
-	packageName = "de.lehrbaum.initiativetracker"
-	exposeObjectWithName = "BuildKonfig"
-
-	defaultConfigs {}
-
-	defaultConfigs("lan") {
-		buildConfigField(STRING, "environment", "lan")
-
-		val host = localProperties.getProperty("backend.lan.host", "\"10.0.2.2\"")
-		val port = localProperties.getProperty("backend.lan.port", "8080")
-		buildConfigField(STRING, "backendHost", host)
-		buildConfigField(INT, "backendPort", port)
-	}
-
-	defaultConfigs("remote") {
-		buildConfigField(STRING, "environment", "remote")
-
-		val host = localProperties.getProperty("backend.remote.host", "\"undefined\"")
-		val port = localProperties.getProperty("backend.remote.port", "443")
-		buildConfigField(STRING, "backendHost", host)
-		buildConfigField(INT, "backendPort", port)
 	}
 }
