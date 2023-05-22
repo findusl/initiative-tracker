68699936c2150b3fb3b14ebb588cc3b02f1bceac
Sebastian <dev@findusl.de>
2023-05-22 12:30:36 +0200

    Error still persists after refactoring like this

--- kmpsharedmodule/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/MainScreen.kt
+++ kmpsharedmodule/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/MainScreen.kt
@@ -1,18 +1,20 @@
 package de.lehrbaum.initiativetracker.ui
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlin.random.Random
 
 @Composable
-fun MainScreen(mainModel: MainModel) {
-	ContentScreen(mainModel.content)
-}
+fun MainScreen(mainModel: MainModel) = ContentScreen(mainModel.content)
 
 class MainModel {
-
     var content by mutableStateOf(getContentModel(0))
 
 	private fun getContentModel(sessionId: Int): ContentModel {
@@ -20,5 +22,31 @@ class MainModel {
 			content = getContentModel(Random.nextInt(10000))
 		}
 	}
+}
 
+@Composable
+fun ContentScreen(contentModel: ContentModel) {
+	Scaffold(topBar = { TopBar(contentModel) },) {
+		Column {
+			Text("This number should match ${contentModel.id}")
+			Button(onClick = contentModel.nextModel) { Text("Generate new id") }
+		}
+	}
+
+	// this is necessary probably compose scoping
+	contentModel.connectionState.collectAsState(false).value.toString()
+}
+
+@Composable
+private fun TopBar(contentModel: ContentModel) =
+	TopAppBar(title = { Text("This number should match ${contentModel.id}") })
+
+data class ContentModel(val id: Int, val nextModel: () -> Unit)  {
+	val connectionState: Flow<Boolean>
+		get() = flow {
+			this.emit(false)
+			delay(10)
+			this.emit(true)
+		}
+			//.flowOn(Dispatchers.IO) //no difference
 }
