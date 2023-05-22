a1ea9387b654816d4c82a2ff064a3549bdfa3098
Sebastian <dev@findusl.de>
2023-05-22 12:25:34 +0200

    Error still persists after refactoring like this

--- kmpsharedmodule/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/ContentModel.kt
+++ kmpsharedmodule/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/ContentModel.kt
@@ -3,12 +3,8 @@ package de.lehrbaum.initiativetracker.ui
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlin.random.Random
 
-data class ContentModel(
-	val id: Int,
-	private val navigateToDifferentId: (Int) -> Unit
-)  {
+data class ContentModel(val id: Int, val nextModel: () -> Unit)  {
 
 	val connectionState: Flow<Boolean>
 		get() = flow {
@@ -17,8 +13,4 @@ data class ContentModel(
 			this.emit(true)
 		}
 			//.flowOn(Dispatchers.IO) //no difference
-
-	fun nextId() {
-		navigateToDifferentId(Random.nextInt(10000))
-	}
 }
