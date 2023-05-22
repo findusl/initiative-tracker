a5b31ccfd6d5ab2092afa6e1f74bed17088bc0ea
Sebastian <dev@findusl.de>
2023-05-22 12:34:15 +0200

    Error still persists after refactoring like this

--- desktop/src/main/kotlin/de/lehrbaum/initiativetracker/Main.kt
+++ desktop/src/main/kotlin/de/lehrbaum/initiativetracker/Main.kt
@@ -1,26 +1,15 @@
 package de.lehrbaum.initiativetracker
 
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import androidx.compose.ui.window.rememberWindowState
 import de.lehrbaum.initiativetracker.ui.MainModel
 import de.lehrbaum.initiativetracker.ui.MainScreen
-import io.github.aakira.napier.DebugAntilog
-import io.github.aakira.napier.Napier
 
 fun main() = application {
-	val windowState = rememberWindowState()
-
-	LaunchedEffect(key1 = this) {
-		// Initialize Logging.
-		Napier.base(DebugAntilog())
-	}
 
 	Window(
 		onCloseRequest = ::exitApplication,
-		state = windowState,
 		title = "InitiativeTracker"
 	) {
 		MaterialTheme {
