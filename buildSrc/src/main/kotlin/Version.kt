27c702c2666663f5fb94d0c496fdfc904f18f592
Sebastian <dev@findusl.de>
2023-06-24 20:42:13 +0200

    Upgrade ktor

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,8 +1,10 @@
 object Version {
+	/* Limited by compose multiplatform.
+	https://github.com/JetBrains/compose-multiplatform/blob/master/VERSIONING.md#kotlin-compatibility*/
 	const val kotlin = "1.8.20"
 	const val kotlinxSerialization = "1.5.1"
 	const val coroutines = "1.7.1"
-	const val ktor = "2.3.0"
+	const val ktor = "2.3.1"
 	const val logback = "1.4.7"
 	const val buildKonfig = "0.13.3"
 	const val mppSettings = "1.0.0"
