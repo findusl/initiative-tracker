f58f127124caa9ff9648eddc9a4471955cf04ea5
Sebastian <dev@findusl.de>
2023-07-29 00:01:29 +0200

    Make hostWebsocketSession mutable, to avoid writing to the sessions map.

--- backend/src/main/kotlin/de/lehrbaum/Session.kt
+++ backend/src/main/kotlin/de/lehrbaum/Session.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 
 internal data class Session(
 	val id: Int,
-	val hostWebsocketSession: DefaultWebSocketServerSession?,
+	var hostWebsocketSession: DefaultWebSocketServerSession?,
 	val combatState: MutableStateFlow<CombatModel>,
 	val serverCommandQueue: Channel<Pair<ServerToHostCommand, (Boolean) -> Unit>> = Channel(capacity = Channel.RENDEZVOUS),
 )
