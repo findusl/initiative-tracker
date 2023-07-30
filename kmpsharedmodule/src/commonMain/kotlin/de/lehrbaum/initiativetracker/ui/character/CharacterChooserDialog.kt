f6139b7b783344ab504d58dd844168df1060f0e0
Sebastian Lehrbaum <sebastian@lehrbaum.de>
2023-07-30 21:16:22 +0200

    Use Dice rolling text field for joining combat

--- kmpsharedmodule/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/character/CharacterChooserDialog.kt
+++ kmpsharedmodule/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/character/CharacterChooserDialog.kt
@@ -14,9 +14,9 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import de.lehrbaum.initiativetracker.bl.Dice
 import de.lehrbaum.initiativetracker.ui.Constants
 import de.lehrbaum.initiativetracker.ui.GeneralDialog
+import de.lehrbaum.initiativetracker.ui.composables.DiceRollingTextField
 import de.lehrbaum.initiativetracker.ui.composables.EditTextField
 import de.lehrbaum.initiativetracker.ui.composables.OkCancelButtonRow
 import de.lehrbaum.initiativetracker.ui.shared.EditFieldViewModel
@@ -54,21 +54,26 @@ private fun ExtraInfoDialog(chosen: CharacterViewModel, onComplete: (CharacterVi
 			color = Color.White
 		) {
 			Column(modifier = Modifier.padding(Constants.defaultPadding)) {
-				val initiativeField = remember(chosen) {
-					EditFieldViewModel(Dice.d20() + (chosen.initiativeMod ?: 0), parseInput = RequiredIntParser)
-				}
-				EditTextField(initiativeField, "Initiative")
+				var initiativeValue by remember { mutableStateOf(0) }
+				val initiativeIsValidNumber = remember { mutableStateOf(false) }
+				DiceRollingTextField(
+					label = "Initiative",
+					initialText = "1#20+${chosen.initiativeMod ?: 0}",
+					onNumberChanged = { initiativeValue = it },
+					textIsValidNumber = initiativeIsValidNumber,
+				)
+				//EditTextField(initiativeField, "Initiative")
 				val currentHpField = remember(chosen) {
 					EditFieldViewModel(chosen.maxHp ?: 0, parseInput = RequiredIntParser)
 				}
 				EditTextField(currentHpField, "Current HP")
 
-				val submittable = remember { derivedStateOf { !initiativeField.hasError && !currentHpField.hasError } }
+				val submittable = remember { derivedStateOf { initiativeIsValidNumber.value && !currentHpField.hasError } }
 
 				OkCancelButtonRow(
 					submittable,
 					onCancel,
-					onSubmit = { onComplete(chosen, initiativeField.value.getOrThrow(), currentHpField.value.getOrThrow()) }
+					onSubmit = { onComplete(chosen, initiativeValue, currentHpField.value.getOrThrow()) }
 				)
 			}
 		}
