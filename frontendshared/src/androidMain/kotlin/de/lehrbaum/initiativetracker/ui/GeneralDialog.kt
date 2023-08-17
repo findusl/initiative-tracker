2f7a32ac4dc912119286f6c65b58adbe5a02583a
Sebastian Lehrbaum <sebastian@lehrbaum.de>
2023-08-17 23:20:20 +0200

    Improve ConfirmDamageDialog. Add surface on mobile in background.
    Part of #91

--- frontendshared/src/androidMain/kotlin/de/lehrbaum/initiativetracker/ui/GeneralDialog.kt
+++ frontendshared/src/androidMain/kotlin/de/lehrbaum/initiativetracker/ui/GeneralDialog.kt
@@ -1,9 +1,12 @@
 package de.lehrbaum.initiativetracker.ui
 
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.Dialog
 
 @Composable
 actual fun GeneralDialog(onDismissRequest: () -> Unit, content: @Composable () -> Unit) {
-    Dialog(onDismissRequest = onDismissRequest, content = content)
+    Dialog(onDismissRequest = onDismissRequest) {
+		Surface(content = content)
+	}
 }
