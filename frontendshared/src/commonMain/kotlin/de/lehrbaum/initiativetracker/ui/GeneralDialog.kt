cc3182487e1dec0afd1f86eaab54c1c266b70c24
Sebastian <dev@findusl.de>
2023-09-06 23:01:23 +0200

    Use new Dialog

--- frontendshared/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/GeneralDialog.kt
+++ frontendshared/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/GeneralDialog.kt
@@ -1,6 +1,12 @@
 package de.lehrbaum.initiativetracker.ui
 
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.Dialog
 
 @Composable
-expect fun GeneralDialog(onDismissRequest: () -> Unit, content: @Composable () -> Unit)
+fun GeneralDialog(onDismissRequest: () -> Unit, content: @Composable () -> Unit) {
+	Dialog(onDismissRequest = onDismissRequest) {
+		Surface(content = content)
+	}
+}
