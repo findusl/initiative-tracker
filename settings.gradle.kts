408dd2e32c1bab0aca9e9b91607360bf3d5b2993
Sebastian Lehrbaum <sebastian@lehrbaum.de>
2023-09-07 21:00:21 +0200

    Clean up Gradle plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,6 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
-    }
-    resolutionStrategy {
-		eachPlugin {
-			if (requested.id.id.startsWith("com.google.cloud.tools.appengine")) {
-				useModule("com.google.cloud.tools:appengine-gradle-plugin:${requested.version}")
-			}
-		}
 	}
 }
 
