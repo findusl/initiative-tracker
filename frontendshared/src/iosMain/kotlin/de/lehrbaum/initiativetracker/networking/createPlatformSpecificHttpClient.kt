e012dcb0286530782bab5fc0cc5882deee71060d
Sebastian Lehrbaum <sebastian@lehrbaum.de>
2023-09-10 14:17:33 +0200

    Move ping responsibility to backend

--- frontendshared/src/iosMain/kotlin/de/lehrbaum/initiativetracker/networking/createPlatformSpecificHttpClient.kt
+++ frontendshared/src/iosMain/kotlin/de/lehrbaum/initiativetracker/networking/createPlatformSpecificHttpClient.kt
@@ -1,10 +1,19 @@
 package de.lehrbaum.initiativetracker.networking
 
+import io.github.aakira.napier.Napier
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.darwin.Darwin
 
 actual fun createPlatformSpecificHttpClient(initializationBlock: HttpClientConfig<*>.() -> Unit): HttpClient =
 	HttpClient(Darwin) {
+		engine {
+			configureSession {
+				Napier.i("Configure session $this")
+			}
+			configureRequest {
+				Napier.i("Configure request $this")
+			}
+		}
 		initializationBlock()
 	}
