5e02a0273b8c20d33323441e8ae060b8daf24082
Sebastian <dev@findusl.de>
2023-10-07 19:01:35 +0200

    Update Versions

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -3,25 +3,25 @@ object Version {
 	https://github.com/JetBrains/compose-multiplatform/blob/master/VERSIONING.md#kotlin-compatibility*/
 	const val kotlin = "1.9.10"
 	const val kotlinxSerialization = "1.6.0"
-	const val kotlinxCollections = "0.3.5"
+	const val kotlinxCollections = "0.3.6"
 	const val coroutines = "1.7.3"
-	const val ktor = "2.3.4"
+	const val ktor = "2.3.5"
 	const val logback = "1.4.11"
 	const val buildKonfig = "0.14.0"
 	const val mppSettings = "1.0.0"
 	const val mockito = "5.4.0"
 	const val napier = "2.6.1"
-	const val openAiClient = "3.4.1"
+	const val openAiClient = "3.5.0"
 	const val kamel = "0.7.3"
 	const val versionPlugin = "0.48.0"
 
 	object JetbrainsCompose {
-		const val foundation = "1.5.1"
+		const val foundation = "1.5.3"
 	}
 	object Android {
-		const val composeBom = "2023.09.00"
+		const val composeBom = "2023.10.00"
 		const val composeCompiler = "1.5.3"
-		const val gradleBuildTools = "8.1.1"
+		const val gradleBuildTools = "8.1.2"
 	}
 	object ApacheCommons {
 		const val compress = "1.24.0"
