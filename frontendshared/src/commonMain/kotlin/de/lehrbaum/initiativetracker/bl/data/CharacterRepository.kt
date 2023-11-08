ab37ac2b9af064aae50c2c2d2c67dc18249fd2d6
Sebastian <dev@findusl.de>
2023-11-08 23:46:51 +0100

    Create extension to avoid explicitly passing serializer

--- frontendshared/src/commonMain/kotlin/de/lehrbaum/initiativetracker/bl/data/CharacterRepository.kt
+++ frontendshared/src/commonMain/kotlin/de/lehrbaum/initiativetracker/bl/data/CharacterRepository.kt
@@ -1,12 +1,9 @@
 package de.lehrbaum.initiativetracker.bl.data
 
 import com.russhwolf.settings.ExperimentalSettingsApi
-import com.russhwolf.settings.serialization.decodeValue
-import com.russhwolf.settings.serialization.encodeValue
 import de.lehrbaum.initiativetracker.bl.model.CharacterModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.builtins.ListSerializer
 
 private const val SETTINGS_NAME = "CHARACTER_PREFERENCES"
 
@@ -15,15 +12,14 @@ private const val SETTINGS_KEY = "CHARACTERS"
 @OptIn(ExperimentalSerializationApi::class, ExperimentalSettingsApi::class)
 class CharacterRepository {
 	private val settings = createSettingsFactory().create(SETTINGS_NAME)
-	private val serializer = ListSerializer(CharacterModel.serializer())
 
 	val characters = MutableStateFlow(loadCharacters())
 
 	private fun loadCharacters(): List<CharacterModel> =
-		settings.decodeValue(serializer, SETTINGS_KEY, emptyList())
+		settings.decodeValue(SETTINGS_KEY, emptyList())
 
 	private fun persistCharacters() =
-		settings.encodeValue(serializer, SETTINGS_KEY, characters.value)
+		settings.encodeValue(SETTINGS_KEY, characters.value)
 
 	fun addCharacter(): CharacterModel {
 		val defaultCharacter = CharacterModel(nextFreeId(), "New Character", initiativeMod = null, maxHp = null)
--- frontendshared/src/commonMain/kotlin/de/lehrbaum/initiativetracker/bl/data/CombatLinkRepository.kt
+++ frontendshared/src/commonMain/kotlin/de/lehrbaum/initiativetracker/bl/data/CombatLinkRepository.kt
@@ -1,21 +1,16 @@
 package de.lehrbaum.initiativetracker.bl.data
 
 import com.russhwolf.settings.ExperimentalSettingsApi
-import com.russhwolf.settings.serialization.decodeValue
-import com.russhwolf.settings.serialization.encodeValue
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.builtins.SetSerializer
 
 private const val SETTINGS_KEY = "links"
 private const val SETTINGS_NAME = "combatlink"
 
-@Suppress("OPT_IN_IS_NOT_ENABLED")
 @OptIn(ExperimentalSerializationApi::class, ExperimentalSettingsApi::class)
 object CombatLinkRepository {
 	private val settings = createSettingsFactory().create(SETTINGS_NAME)
-	private val serializer = SetSerializer(CombatLink.serializer())
 	val combatLinks = MutableStateFlow(loadCombatLinks())
 
 	fun addCombatLink(combatLink: CombatLink) {
@@ -29,10 +24,10 @@ object CombatLinkRepository {
 	}
 
 	private fun persistCombatLinks() =
-		settings.encodeValue(serializer, SETTINGS_KEY, combatLinks.value)
+		settings.encodeValue(SETTINGS_KEY, combatLinks.value)
 
 	private fun loadCombatLinks() =
-		settings.decodeValue(serializer, SETTINGS_KEY, emptySet())
+		settings.decodeValue(SETTINGS_KEY, emptySet<CombatLink>())
 }
 
 @Serializable
