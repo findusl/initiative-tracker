43492fafab240f802abf6b94d84a4e9dbf28d89f
Sebastian Lehrbaum <sebastian@lehrbaum.de>
2023-11-22 13:04:20 +0100

    Remove versions plugin
    currently not working

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,4 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-import java.util.*
-
 plugins {
-	// Plugin helps find available updates for dependencies https://github.com/ben-manes/gradle-versions-plugin
-	alias(libs.plugins.ben.manes.versions)
-
 	// this is necessary to avoid the plugins loaded multiple times
 	alias(libs.plugins.kotlin.multiplatform) apply false
 	alias(libs.plugins.jetbrains.compose) apply false
@@ -38,20 +32,4 @@ allprojects {
 
 tasks.register("clean", Delete::class) {
 	delete(rootProject.layout.buildDirectory)
-}
-
-// What kind of dependencies updates interest me https://github.com/ben-manes/gradle-versions-plugin
-tasks.withType<DependencyUpdatesTask> {
-	rejectVersionIf {
-		!isStable(candidate.version) && isStable(currentVersion)
-	}
-}
-
-/** Helper for dependency update filters */
-fun isStable(version: String): Boolean {
-	val stableKeyword = listOf("RELEASE", "FINAL", "GA")
-		.any { version.uppercase(Locale.getDefault()).contains(it) }
-	val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-	val isStable = stableKeyword || regex.matches(version)
-	return isStable
 }
