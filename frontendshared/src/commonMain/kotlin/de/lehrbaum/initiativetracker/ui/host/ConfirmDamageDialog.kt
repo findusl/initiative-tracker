9c17a9f4ce3e11505bcfd4a2443a759cf87ce91e
Sebastian Lehrbaum <sebastian@lehrbaum.de>
2023-11-27 17:04:52 +0100

    Add confirm damage with number keys

--- frontendshared/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/host/ConfirmDamageDialog.kt
+++ frontendshared/src/commonMain/kotlin/de/lehrbaum/initiativetracker/ui/host/ConfirmDamageDialog.kt
@@ -8,7 +8,16 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.input.key.type
 import androidx.compose.ui.unit.dp
 import de.lehrbaum.initiativetracker.bl.DamageDecision
 import de.lehrbaum.initiativetracker.bl.DamageDecision.DOUBLE
@@ -29,16 +38,35 @@ fun ConfirmDamageDialog(
 	onDamageApplied: (DamageDecision) -> Unit,
 	onDismiss: () -> Unit
 ) {
+	val focusRequester = remember(options) { FocusRequester() }
 	GeneralDialog(
 		onDismissRequest = onDismiss
 	) {
 		Column(
-			modifier = Modifier.padding(16.dp),
-			verticalArrangement = Arrangement.spacedBy(8.dp)
+			modifier = Modifier
+				.padding(16.dp)
+				.focusRequester(focusRequester)
+				.onKeyEvent {  keyEvent ->
+					if (keyEvent.type != KeyEventType.KeyUp) false
+					else {
+						val index = when (keyEvent.key) {
+							Key.One -> 0
+							Key.Two -> 1
+							Key.Three -> 2
+							Key.Four -> 3
+							else -> null
+						}
+						index?.let {
+							onDamageApplied(DamageDecision.entries[it])
+							true
+						} ?: false
+					}
+				},
+			verticalArrangement = Arrangement.spacedBy(8.dp),
 		) {
 			Text("Apply ${options.damage} Damage to ${options.targetName}", style = MaterialTheme.typography.h6)
 			Text("Probable source: ${options.sourceName}", style = MaterialTheme.typography.subtitle2)
-			DamageDecision.values().forEach { option ->
+			DamageDecision.entries.forEach { option ->
 				Button(
 					onClick = { onDamageApplied(option) },
 					modifier = Modifier.fillMaxWidth()
@@ -47,6 +75,10 @@ fun ConfirmDamageDialog(
 				}
 			}
 		}
+	}
+
+	LaunchedEffect(options) {
+		focusRequester.requestFocus()
 	}
 }
 
