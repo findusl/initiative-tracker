11f927848ba56da831d08efb1032336b7d1e4aa4
Sebastian Lehrbaum <sebastian@lehrbaum.de>
2023-12-26 13:23:24 +0100

    Improve logging
    - Add Log in the command transcription
    - Reduce log level in the http client
    - Remove star import

--- frontendshared/src/commonMain/kotlin/de/lehrbaum/initiativetracker/GlobalInstances.kt
+++ frontendshared/src/commonMain/kotlin/de/lehrbaum/initiativetracker/GlobalInstances.kt
@@ -1,7 +1,11 @@
 package de.lehrbaum.initiativetracker
 
 import de.lehrbaum.initiativetracker.bl.data.GeneralSettingsRepository
-import de.lehrbaum.initiativetracker.networking.*
+import de.lehrbaum.initiativetracker.networking.BackendNetworkClient
+import de.lehrbaum.initiativetracker.networking.BestiaryNetworkClient
+import de.lehrbaum.initiativetracker.networking.BestiaryNetworkClientImpl
+import de.lehrbaum.initiativetracker.networking.OpenAiNetworkClient
+import de.lehrbaum.initiativetracker.networking.createDefaultHttpClient
 
 /**
  * Should replace with dependency injection in the long run.
