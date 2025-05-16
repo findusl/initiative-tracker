11ece2a674f9c4e179e3e74c9fa70ff21b3b3e27
Sebastian Lehrbaum <sebastian.lehrbaum@check24.de>
2025-05-16 18:58:03 +0200

    Setting option for openai api key

--- frontendshared/src/commonMain/kotlin/de/lehrbaum/initiativetracker/GlobalInstances.kt
+++ frontendshared/src/commonMain/kotlin/de/lehrbaum/initiativetracker/GlobalInstances.kt
@@ -4,7 +4,7 @@ import de.lehrbaum.initiativetracker.data.GeneralSettingsRepository
 import de.lehrbaum.initiativetracker.networking.BackendNetworkClient
 import de.lehrbaum.initiativetracker.networking.BestiaryNetworkClient
 import de.lehrbaum.initiativetracker.networking.BestiaryNetworkClientImpl
-import de.lehrbaum.initiativetracker.networking.OpenAiNetworkClient
+import de.lehrbaum.initiativetracker.networking.OpenAiNetworkClientProvider
 import de.lehrbaum.initiativetracker.networking.createDefaultHttpClient
 
 /**
@@ -16,6 +16,6 @@ object GlobalInstances {
     val httpClient by lazy { createDefaultHttpClient() }
 	val backendNetworkClient by lazy { BackendNetworkClient(httpClient) }
 	val bestiaryNetworkClient: BestiaryNetworkClient by lazy { BestiaryNetworkClientImpl(httpClient) }
-	val openAiNetworkClient by lazy { BuildKonfig.openaiApiKey?.let { OpenAiNetworkClient(it) } }
 	val generalSettingsRepository by lazy { GeneralSettingsRepository() }
+	val openAiNetworkClientProvider by lazy { OpenAiNetworkClientProvider(generalSettingsRepository) }
 }
